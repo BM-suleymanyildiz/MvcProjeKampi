5e922fa85b8281e35b8830dd0ee1c1ca91cbc791
Suleyman Yildiz <suleymanyildiz491@gmail.com>
2025-05-12 15:02:32 +0300

    ..

--- MvcProjeKampi/Controllers/CategoryController.cs
+++ MvcProjeKampi/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,33 @@ namespace MvcProjeKampi.Controllers
 {
     public class CategoryController : Controller
     {
-        
+        CategoryManager cm = new CategoryManager();
+
         public ActionResult Index()
         {
             return View();
         }
+
+        public ActionResult GetCategoryList()
+        {
+            var categoryvalues = cm.GetAllBL();
+            return View(categoryvalues);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            cm.CategoryAddBL(p);
+            return RedirectToAction("GetCategoryList");
+        }
+
+
     }
 }
